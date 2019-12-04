06adbbb32d84f077918cab6b4516e6ff472aea2a
vandhana <>
2019-12-04 13:45:38 +0530

    asdasd

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
-      { from: Path.resolve(__dirname, '../public'), to: 'public' }
+      { from: Path.resolve(__dirname, '../src/assets'), to: 'src/assets' }
     ]),
     new HtmlWebpackPlugin({
       template: Path.resolve(__dirname, '../src/index.html')
